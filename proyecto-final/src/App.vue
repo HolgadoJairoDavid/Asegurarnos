df204f2e23651aff6f038643057a928a9e63ddbd
EAguirre <aguirreernesto25@gmail.com>
2023-06-08 16:40:26 -0300

    Funcion Nombre

--- proyecto-final/src/App.vue
+++ proyecto-final/src/App.vue
@@ -2,6 +2,11 @@
 import ErnestoEditaAcáVue from "./components/ErnestoEditaAcá.vue";
 import NicoEditaAcáVue from "./components/NicoEditaAcá.vue";
 import AuferTambiénInvitadoVue from "./components/AuferTambiénInvitado.vue";
+
+const nombre = () => {
+  return "Mi Nombre es Ernesto"
+};
+
 </script>
 <template>
   <div>Bienvenidos al Proyecto Final</div>
