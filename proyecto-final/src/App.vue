ef8a42931710128aa83906da77709cbb3fcf951d
HolgadoJairoDavid <jairodavidholgado@gamil.com>
2023-06-08 16:01:08 -0300

    simulacro de oproyecto final

---
+++ proyecto-final/src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+import ErnestoEditaAcáVue from "./components/ErnestoEditaAcá.vue";
+import NicoEditaAcáVue from "./components/NicoEditaAcá.vue";
+import AuferTambiénInvitadoVue from "./components/AuferTambiénInvitado.vue";
+</script>
+<template>
+  <div>Bienvenidos al Proyecto Final</div>
+</template>
+
+<style scoped>
+* {
+  background-color: red;
+  color: white;
+}
+</style>
